5f6724f536951ec24be821ce22367d30cbb70c03
Million Hailu <million.hailu@capeanalytics.com>
2024-10-27 01:06:14 -0400

    Beautificationa additions

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -47,7 +47,7 @@ const CardDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <p
     ref={ref}
-    className={cn("text-sm text-muted-foreground", className || '')} // Provide a default value if className is undefined
+    className={cn("text-sm text-muted-foreground", className || '')}
     {...props}
   />
 ))
@@ -57,7 +57,7 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className || '')} {...props} /> // Provide a default value if className is undefined
+  <div ref={ref} className={cn("p-6 pt-0", className || '')} {...props} />
 ))
 CardContent.displayName = "CardContent"
 
@@ -67,7 +67,7 @@ const CardFooter = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex items-center p-6 pt-0", className || '')} // Provide a default value if className is undefined
+    className={cn("flex items-center p-6 pt-0", className || '')}
     {...props}
   />
 ))
